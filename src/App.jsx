73c4bebc25907dea8f529b1069e8410024624752
iftekher <iftekherakash6@gmail.com>
2025-11-09 13:33:28 +0600

    tailwind+DaisyUi setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">FOOD SHAREING</h1>
+      <div className="avatar">
+        <div className="w-24 rounded-xl">
+          <img src="https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp" />
+        </div>
+      </div>
+      <div className="avatar">
+        <div className="w-24 rounded-full">
+          <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
