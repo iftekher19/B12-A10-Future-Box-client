33863c933ac96b07467979cc731b2e5ab3888b2a
iftekher <iftekherakash6@gmail.com>
2025-11-11 05:20:14 +0600

    error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Card from '../Components/Card';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import { Link } from 'react-router';
 
 const Error = () => {
     return (
-        <div>
-            <h3>This is Error Page</h3>
+      <div>
+        <Navbar />
+        <div className="flex flex-col items-center justify-center min-h-[80vh] px-4  ">
+          <Card />
+          <Link
+            to="/"
+            className="btn bg-[#00a1b7] text-white border-none hover:bg-primary-focus"
+          >
+            Back to Home
+          </Link>
         </div>
+
+        <Footer />
+      </div>
     );
 };
 
--- src/axios.config.js
+++ src/axios.config.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // replace <your-vercel-server-url> when deployed
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL:
+    "https://tabletogether-6cxgl7skd-md-iftekher-ahmeds-projects.vercel.app",
 });
 
 export default axiosInstance;
